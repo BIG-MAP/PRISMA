ded30dbd274d86bc4d6861250d774ab9fb1799b1
= <=>
2023-09-14 21:19:21 +0200

    Added sidebar and plot to prototype

--- gui2/prototype.py
+++ gui2/prototype.py
@@ -1,9 +1,26 @@
 import streamlit as st
-
+import pandas as pd
+import plotly.express as px
+from io import BytesIO
+from PIL import Image
+import requests
 
 
 
 st.title("Prototype for views")
+
+response = requests.get("https://raw.githubusercontent.com/BIG-MAP/PRISMA/main/docs/figures/logo.png",
+                        stream=True)
+if response.status_code == 200:
+        logo = BytesIO(response.content)
+        logo = Image.open(logo).convert('RGB')
+
+st.sidebar.image(image=logo)
+st.sidebar.button("PRISMA", use_container_width= True)
+st.sidebar.button("Smoothing", use_container_width= True)
+st.sidebar.button("Baseline Correction", use_container_width= True)
+st.sidebar.button("Peak Fitting", use_container_width= True)
+st.sidebar.button("Process and Save", use_container_width= True)
 
 donwsamp_col1, donwsamp_col2, donwsamp_col3 = st.columns(3)
 
@@ -23,3 +40,8 @@ smooth_col1.write('Smoothing window')
 smooth_param = smooth_col2.slider('', 3, 100, 2, label_visibility="collapsed")
 if smooth_col3.button('*i*', key="smoothing"):
     st.toast("Applies the XX algorithm to smooth a spectrum. See [Documentation](https://www.google.com/)")
+
+
+df = pd.read_csv("ExxonMobil_CaCO3_LLDPE.txt", sep="\t", names= ["x","y"])
+fig = px.scatter(df, x="x", y="y")
+st.plotly_chart(fig, use_container_width=True)
